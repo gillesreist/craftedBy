208f859090a618946d1b0a475336de1c8730c1d3
gilles <gilles.reist@gmail.com>
2024-02-07 13:44:29 +0100

    #4 fortify activated

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
 
-Route::apiResource('products', ProductController::class);
+Route::apiResource('products', ProductController::class)->middleware('auth:sanctum');
 Route::apiResource('attributes', AttributeController::class);
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('materials', MaterialController::class);
