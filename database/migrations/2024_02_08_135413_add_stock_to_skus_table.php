08f693f34466bf39aa07447c4c7ca322f673ece7
gilles <gilles.reist@gmail.com>
2024-02-08 15:07:18 +0100

    #8 stock column added to skus table

---
+++ database/migrations/2024_02_08_135413_add_stock_to_skus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Sku;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('skus', function (Blueprint $table) {
+            $table->integer('stock');
+        });
+
+        // update existing entries
+        $skus = Sku::all();
+        foreach ($skus as $sku) {
+            $sku->update(['stock' => rand(0, 50)]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('skus', function (Blueprint $table) {
+            $table->dropColumn('stock');
+        });
+    }
+};
