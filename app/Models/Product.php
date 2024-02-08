1e7c88411e68ab9c5e9609b62837e3ad3fa00d34
gilles <gilles.reist@gmail.com>
2024-02-08 16:19:53 +0100

    #9 slug column added to product table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,10 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids,HasSlug;
 
     protected $guarded = [];
 
@@ -42,6 +44,14 @@ class Product extends Model
         return $this->belongsToMany(Material::class);
     }
 
-
+      /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 
 }
