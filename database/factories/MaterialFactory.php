88d03d6fb227da9da72e7e7818e8860fadb35550
gilles <gilles.reist@gmail.com>
2024-05-02 10:56:44 +0200

    Add unique to material and customization factory

--- database/factories/MaterialFactory.php
+++ database/factories/MaterialFactory.php
@@ -17,7 +17,7 @@ class MaterialFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->word(),
+            'name' => fake()->unique()->word(),
         ];
     }
 }
